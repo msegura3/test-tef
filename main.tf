767ea73d6d215df99a9d3608145da65c3f47ff77
Marco Segura <segura.torres@gmail.com>
2018-11-20 11:48:43 -0300

    dummy

--- main.tf
+++ main.tf
@@ -1,3 +1,57 @@
 
 provider "google" {
 }  
+
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+data "google_service_account" "myaccount" {
+  account_id = "myaccount-id"
+}
+
+resource "google_service_account_key" "mykey" {
+  service_account_id = "${data.google_service_account.myaccount.name}"
+}
+
+resource "kubernetes_secret" "google-application-credentials" {
+  metadata {
+    name = "google-application-credentials"
+  }
+  data {
+    credentials.json = "${base64decode(google_service_account_key.mykey.private_key)}"
+  }
+  
