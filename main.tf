a85b29e65cba49bf9f96599ff1f47166270a4511
Marco Segura <segura.torres@gmail.com>
2018-11-20 11:52:41 -0300

    test

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 
 provider "google" {
-}  
-
+} 
 
 resource "google_compute_instance" "default" {
   name         = "test"
@@ -39,19 +38,46 @@ resource "google_compute_instance" "default" {
   }
 }
 
-data "google_service_account" "myaccount" {
-  account_id = "myaccount-id"
+resource "google_container_cluster" "primary" {
+  name               = "marcellus-wallace"
+  zone               = "us-central1-a"
+  initial_node_count = 3
+
+  additional_zones = [
+    "us-central1-b",
+    "us-central1-c",
+  ]
+
+  master_auth {
+    username = "mr.yoda"
+    password = "adoy.rm"
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels {
+      foo = "bar"
+    }
+
+    tags = ["foo", "bar"]
+  }
 }
 
-resource "google_service_account_key" "mykey" {
-  service_account_id = "${data.google_service_account.myaccount.name}"
+# The following outputs allow authentication and connectivity to the GKE Cluster.
+output "client_certificate" {
+  value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
 }
 
-resource "kubernetes_secret" "google-application-credentials" {
-  metadata {
-    name = "google-application-credentials"
-  }
-  data {
-    credentials.json = "${base64decode(google_service_account_key.mykey.private_key)}"
-  }
-  
+output "client_key" {
+  value = "${google_container_cluster.primary.master_auth.0.client_key}"
+}
+
+output "cluster_ca_certificate" {
+  value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
+}
